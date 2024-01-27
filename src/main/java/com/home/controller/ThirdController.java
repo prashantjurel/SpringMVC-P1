59c45404543ec75f74cab3c27f8a6bc703379ba5
prashantjurel <prashantjurel@outlook.com>
2024-01-27 17:48:16 +0530

    Added dispatcher servlet with different name using init-param in web.xml file

--- src/main/java/com/home/controller/ThirdController.java
+++ src/main/java/com/home/controller/ThirdController.java
@@ -5,12 +5,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequestMapping("/3")
 public class ThirdController {
 	
 	@ResponseBody
 	@RequestMapping("/third")
 	public String thirdControllerString(){
 		return "This is the Third Controller String";
+	}
+	
+	@ResponseBody
+	@RequestMapping("/other")
+	public String otherControllerString(){
+		return "This is the Other Controller String";
 	}
 
 }
